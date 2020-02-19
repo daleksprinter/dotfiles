ef23feb5d4b3c42d3d3239354276a910e86d0fd4
daleksprinter <g2119022@fun.ac.jp>
2020-02-19 17:53:27 +0900

    add resize-pane setting to .tmux.conf

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # symbolic link
 ln -sfv $PWD/.vimrc $HOME/.vimrc
-ln -sfv $PWD/.tmux.conf $HOME/.tmux.confinstall 
+ln -sfv $PWD/.tmux.conf $HOME/.tmux.conf 
 ln -sfv $PWD/.vim $HOME/.vim
 ln -sfv $PWD/config.fish $HOME/.config/fish/config.fish
