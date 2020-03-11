dcd375c659c36f9b47e3d5548e58050bfad3ec19
daleksprinter <g2119022@fun.ac.jp>
2020-03-11 11:50:35 +0900

    update install.sh for installing vim plugins

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ ln -sfv $PWD/.vimrc $HOME/.vimrc
 ln -sfv $PWD/.tmux.conf $HOME/.tmux.conf 
 ln -sfv $PWD/.vim $HOME/.vim
 ln -sfv $PWD/config.fish $HOME/.config/fish/config.fish
+
+# install vim plugins
+vim -c 'PlugInstall | q | q'
