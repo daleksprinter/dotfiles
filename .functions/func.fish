c420b88954c7cc740d3cd306d5aec97db126199f
daleksprinter <g2119022@fun.ac.jp>
2020-05-21 17:54:12 +0900

    update fvim, repo

--- .functions/func.fish
+++ .functions/func.fish
@@ -1,10 +1,13 @@
 function fvim
 	set gdir (git rev-parse --show-toplevel)
-	set file (find $gdir -type f | fzf --preview 'bat {}')
+	set len (echo $gdir/ | wc -c | sed 's/ //g')
+	set bat (echo bat $gdir/{})
+	set file (find $gdir -type f | cut -c $len- | fzf --preview $bat)
 	vim $file
 end
 
 function repo
-	set dir (ghq list -p | fzf --preview 'ls -a {}')
-	cd $dir
+	set len (ghq root | wc -c | sed 's/ //g')
+	set dir (ghq list -p | cut -c $len- | fzf --preview 'ls -a {}')
+	cd (ghq root)$dir
 end
