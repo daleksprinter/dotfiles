65aad30f9fc6eeb0892d0c6674e60dabf70cfb66
daleksprinter <g2119022@fun.ac.jp>
2020-05-21 15:38:04 +0900

    add fvim function, modify repo function

--- .functions/func.fish
+++ .functions/func.fish
@@ -1,33 +1,10 @@
-function cd-git-root
-	set err_msg "No git root found, This dir may not be under ghq root"
-
-	set dir (pwd)
-	if [ $dir = / ]
-		echo $err_msg
-		return
-	end
-
-	set -a path (string split / $dir)
-	set repos (ghq list --full-path)
-	
-    while true
-	 if [ (count $path) -eq 1 ]
-	     echo $err_msg
-		 return
-	 end
-
-	 set now_dir (string join / $path)
-	 for x in $repos
-
-		 if [ $now_dir = $x ] 
-			 cd $x
-			 return
-		 end
-	 end
-	 set cnt (count $path)
-	 set cnt (expr $cnt - 1)
-	 set path $path[1..$cnt]
-	end
+function fvim
+	set gdir (git rev-parse --show-toplevel)
+	set file (find $gdir -type f | fzf --preview 'bat {}')
+	vim $file
 end
 
-alias gr cd-git-root
+function repo
+	set dir (ghq list -p | fzf --preview 'ls -a {}')
+	cd $dir
+end
--- config.fish
+++ config.fish
@@ -2,14 +2,11 @@ set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 
 alias rm 'rmtrash'
-alias repos='ghq list -p | peco'
-alias repo='cd (repos)'
-alias hs='history | peco'
 
 alias ll 'ls -la'
-alias br='git branch  | peco | xargs git checkout'
 
 set -U FZF_LEGACY_KEYBINDINGS 0
+
 rbenv init - | source
 set -x PATH $HOME/.rbenv/bin $PATH
 set -x PATH /usr/local/opt/mysql-client/bin $PATH
