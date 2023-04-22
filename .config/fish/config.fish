c4eb71e50e5af6a72b4dac99c0e361837eccb9a1
r-sekine <ca01376@cartahd.com>
2023-04-22 18:32:28 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,28 +1,9 @@
-source $HOME/.config/fish/alias.fish
-source $HOME/.config/fish/env.fish
-source ~/.functions/func.fish
-source $HOME/.config/fish/fzf.fish
-source $HOME/.config/fish/theme.fish
-source $HOME/.config/fish/complete.fish
-
-eval (scalaenv init - | source)
-
-bind \cy __tmux_session_fzf
-function __tmux_session_fzf
-    set issession ( echo $TMUX | cut -d ',' -f 2)
-    set session (tmux ls | fzf | cut -d ':' -f 1)
-    test -n $issession && tmux switch-client -t $session || tmux a -t $session
-end
+source $HOME/.config/fish/mycnf/alias.fish
+source $HOME/.config/fish/mycnf/env.fish
+source $HOME/.config/fish/mycnf/fzf.fish
+source $HOME/.config/fish/mycnf/theme.fish
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/r-sekine/google-cloud-sdk/path.fish.inc' ]; . '/Users/r-sekine/google-cloud-sdk/path.fish.inc'; end
-
-
-function nvm
-   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
-
-set -x NVM_DIR ~/.nvm
-nvm use default --silent
 
 source /Users/r-sekine/.docker/init-fish.sh || true # Added by Docker Desktop
--- .config/fish/fzf.fish
+++
@@ -1,2 +0,0 @@
-set -U FZF_LEGACY_KEYBINDINGS 0
-set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
