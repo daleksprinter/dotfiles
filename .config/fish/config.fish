4892a761ae0175480894e4088af98ceac5dcbc46
r-sekine <ca01376@cartahd.com>
2023-04-22 18:34:47 +0900

    fix

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source $HOME/.config/fish/mycnf/alias.fish
+source $HOME/.config/fish/mycnf/env.fish
+source $HOME/.config/fish/mycnf/fzf.fish
+source $HOME/.config/fish/mycnf/theme.fish
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/r-sekine/google-cloud-sdk/path.fish.inc' ]; . '/Users/r-sekine/google-cloud-sdk/path.fish.inc'; end
+
+source /Users/r-sekine/.docker/init-fish.sh || true # Added by Docker Desktop
