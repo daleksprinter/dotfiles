0ce7e8c544e1bad9165f23beebb9fe7ed6b6f30d
r-sekine <ca01376@cartahd.com>
2023-04-22 21:08:45 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ source $HOME/.config/fish/mycnf/alias.fish
 source $HOME/.config/fish/mycnf/env.fish
 source $HOME/.config/fish/mycnf/fzf.fish
 source $HOME/.config/fish/mycnf/theme.fish
+source $HOME/.config/fish/mycnf/language.fish
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/r-sekine/google-cloud-sdk/path.fish.inc' ]; . '/Users/r-sekine/google-cloud-sdk/path.fish.inc'; end
---
+++ .config/fish/mycnf/language.fish
@@ -0,0 +1,27 @@
+#Scala
+eval (scalaenv init - | source)
+
+#Ruby
+set -x PATH $HOME/.rbenv/bin $PATH
+eval (rbenv init - | source)
+
+#Node.js
+
+#Python
+set -x PATH $PATH /Users/r-sekine/.pyenv/versions
+eval (pyenv init - | source)
+
+#PHP
+set -x PATH $HOME/.phpenv/bin $PATH
+eval (phpenv init - | source)
+
+# go
+set -x GO111MODULE on
+set -x GOPATH $HOME/go
+set -x GOENV_ROOT $HOME/.goenv
+set -x PATH $GOENV_ROOT/bin $PATH
+eval (goenv init - | source)
+
+#Java
+set -x JAVA_HOME  /Library/Java/JavaVirtualMachines/openjdk-8.jdk/Contents/Home
+set -x PATH $JAVA_HOME $PATH
