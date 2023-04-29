0131851bc05420b8cf6f215234b253430c61f6ea
r-sekine <ca01376@cartahd.com>
2023-04-29 09:13:03 +0900

    fix

--- fish.mk
+++ fish.mk
@@ -1,12 +1,18 @@
-TARGETS:=$(HOME)/.config/fish
+TARGETS:=$(HOME)/.config/fish/mycnf $(HOME)/.config/fish/config.fish
 
 all: $(TARGETS)
 
 $(HOME)/.config:
 	mkdir -p $@
 
-$(HOME)/.config/fish: fish $(HOME)/.config
+$(HOME)/.config/fish: $(HOME)/.config
+	mkdir -p $@
+
+$(HOME)/.config/fish/mycnf: fish/mycnf $(HOME)/.config/fish
 	cp -r $< $@
+
+$(HOME)/.config/fish/config.fish: fish/config.fish $(HOME)/.config/fish
+	cp $< $@
 
 clean:
 	rm -rf $(TARGETS)
