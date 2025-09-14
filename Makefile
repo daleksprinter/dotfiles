1f3d5372596c93af80ff43bd50819cf13f7750fe
r-sekine <ca01376@cartahd.com>
2025-09-14 14:26:44 +0900

    fix

--- Makefile
+++
@@ -1,4 +0,0 @@
-TARGETS:=brew fish fisher git idea vim tmux
-
-all:
-	for target in $(TARGETS); do make -C src/$$target all; done
--- src/fish/conf/.config/fish/conf.d/alias.fish
+++ src/fish/conf/.config/fish/conf.d/alias.fish
@@ -1,6 +1,3 @@
-#basic
-alias ll 'ls -la'
-
 # cd alias
 alias desktop 'cd ~/Desktop'
 alias download 'cd ~/Downloads'
@@ -11,8 +8,6 @@ alias ga='git add'
 alias gap='git add -p'
 alias gb='git branch'
 alias gcm='git commit -m'
-alias gd='git diff'
-alias gdc='git diff --cached'
 alias gl='git log --pretty=format:"%C(yellow)%h%Creset %C(magenta)%ci%Creset%n%C(cyan)%an <%ae>%Creset%n%B"'
 alias gp='git push'
 alias gs='git status'
@@ -21,24 +16,6 @@ alias gs='git status'
 alias date='gdate'
 alias grep='ggrep'
 
-#self scripted
-alias propen='gh pr view -w'
-
 #shortcut
 alias q='exit'
 alias gget='ghq get -l'
-
-#zgok
-function ssmzgokfzf
-  set -l target (aws --profile zucks-zgok --region ap-northeast-1 ec2 describe-instances --filter 'Name=instance-state-name,Values=running' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`] | [0].Value,InstanceId: InstanceId}' --output json | jq -c .[] | sort | fzf --exit-0 | jq -r .InstanceId)
-  if test -n "$target"
-    aws --profile zucks-zgok --region ap-northeast-1 ssm start-session --target $target
-  end
-end
-
-function ssmadnetfzf
-  set -l target (aws --profile zgok-adnet --region ap-northeast-1 ec2 describe-instances --filter 'Name=instance-state-name,Values=running' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`] | [0].Value,InstanceId: InstanceId}' --output json | jq -c .[] | sort | fzf --exit-0 | jq -r .InstanceId)
-  if test -n "$target"
-    aws --profile zgok-adnet --region ap-northeast-1 ssm start-session --target $target
-  end
-end
